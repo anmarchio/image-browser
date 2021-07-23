5b1ac8843eeac04dd2ec132b46b51505d5229323
mara <andreas.margraf@igcv.fraunhofer.de>
2021-07-23 17:30:37 +0200

    add unittests

--- src/ImageBrowserModel/MainFrame.cpp
+++ src/ImageBrowserModel/MainFrame.cpp
@@ -7,8 +7,6 @@
 #include <wx/filedlg.h>
 #include <wx/wfstream.h>
 
-#include "../../../../wxWidgets-3.1.4/include/wx/defs.h"
-
 using std::string;
 using std::experimental::filesystem::directory_iterator;
 
